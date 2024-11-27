0e260849385be38e2519968c93f0ff05a44ff743
Miguel <35431824+Mig-uel@users.noreply.github.com>
2024-11-27 02:50:35 -0500

    Implement isPalindrome function to check if a string is a palindrome

--- 01-basic-challenges-1/08-palindrome/palindrome.js
+++ 01-basic-challenges-1/08-palindrome/palindrome.js
@@ -1,3 +1,15 @@
-function isPalindrome() {}
+function isPalindrome(str) {
+  let reversed = ''
 
-module.exports = isPalindrome;
+  for (let i = str.length; i > 0; i--) {
+    reversed += str[i - 1]
+  }
+
+  str = str.replace(/[^a-z0-9]/g, '')
+  reversed = reversed.replace(/[^a-z0-9]/g, '')
+
+  if (str === reversed) return true
+  else return false
+}
+
+module.exports = isPalindrome
