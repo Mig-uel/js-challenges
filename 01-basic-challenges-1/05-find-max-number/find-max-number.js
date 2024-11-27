c19979e5271032ed8254ed4dada8e17991e06473
Miguel <35431824+Mig-uel@users.noreply.github.com>
2024-11-27 02:24:28 -0500

    Implement findMaxNumber function to return the greatest number from an array

--- 01-basic-challenges-1/05-find-max-number/find-max-number.js
+++ 01-basic-challenges-1/05-find-max-number/find-max-number.js
@@ -1,3 +1,11 @@
-function findMaxNumber() {}
+function findMaxNumber(numbers) {
+  let greatest = numbers[0]
 
-module.exports = findMaxNumber;
+  for (let i = 0; i < numbers.length; i++) {
+    if (numbers[0] > greatest) greatest = numbers[0]
+  }
+
+  return greatest
+}
+
+module.exports = findMaxNumber
