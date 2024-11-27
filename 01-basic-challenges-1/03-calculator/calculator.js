40a8a33f526aa541974257320e10914c974a4142
Miguel <35431824+Mig-uel@users.noreply.github.com>
2024-11-27 02:15:26 -0500

    Implement calculator function to perform basic arithmetic operations

--- 01-basic-challenges-1/03-calculator/calculator.js
+++ 01-basic-challenges-1/03-calculator/calculator.js
@@ -1,3 +1,19 @@
-function calculator() {}
+function calculator(num1, num2, operator) {
+  if (
+    operator !== '+' &&
+    operator !== '-' &&
+    operator !== '*' &&
+    operator !== '/'
+  )
+    throw new Error('Invalid operator')
 
-module.exports = calculator;
+  if (operator === '+') return num1 + num2
+
+  if (operator === '-') return num1 - num2
+
+  if (operator === '*') return num1 * num2
+
+  if (operator === '/') return num1 / num2
+}
+
+module.exports = calculator
