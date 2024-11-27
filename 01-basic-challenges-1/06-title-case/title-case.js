ac2e28ef455ae81d699c957fbfa3b7928046d25a
Miguel <35431824+Mig-uel@users.noreply.github.com>
2024-11-27 02:35:33 -0500

    Implement titleCase function to capitalize the first letter of each word in a string

--- 01-basic-challenges-1/06-title-case/title-case.js
+++ 01-basic-challenges-1/06-title-case/title-case.js
@@ -1,3 +1,15 @@
-function titleCase() {}
+function titleCase(string) {
+  const splitted = string.split(' ')
+  const modified = []
 
-module.exports = titleCase;
+  for (let i = 0; i < splitted.length; i++) {
+    const cap = splitted[i][0].toUpperCase()
+    const low = splitted[i].slice(1, splitted[i].length)
+
+    modified.push(cap + low)
+  }
+
+  return modified.join(' ')
+}
+
+module.exports = titleCase
