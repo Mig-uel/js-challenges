8b7d34310deb9a1e0aea8b43f41f651987fa71d6
Miguel <35431824+Mig-uel@users.noreply.github.com>
2024-11-27 02:58:51 -0500

    Refactor isPalindrome function to normalize input string before comparison

--- 01-basic-challenges-1/08-palindrome/palindrome.js
+++ 01-basic-challenges-1/08-palindrome/palindrome.js
@@ -1,12 +1,11 @@
 function isPalindrome(str) {
+  str = str.toLowerCase().replace(/[^a-z0-9]/g, '')
+
   let reversed = ''
 
   for (let i = str.length; i > 0; i--) {
     reversed += str[i - 1]
   }
-
-  str = str.replace(/[^a-z0-9]/g, '')
-  reversed = reversed.replace(/[^a-z0-9]/g, '')
 
   if (str === reversed) return true
   else return false
