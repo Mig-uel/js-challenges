93ba8fc93b193abcb21e4d56e6f04b78ebac4488
Miguel <35431824+Mig-uel@users.noreply.github.com>
2024-11-27 09:50:46 -0500

    Implement countVowels function to count the number of vowels in a given string

--- 01-basic-challenges-1/09-count-vowels/count-vowels.js
+++ 01-basic-challenges-1/09-count-vowels/count-vowels.js
@@ -1,3 +1,14 @@
-function countVowels() {}
+function countVowels(str) {
+  let count = 0
+  let vowels = ['a', 'e', 'i', 'o', 'u']
 
-module.exports = countVowels;
+  for (let i = 0; i < str.length; i++) {
+    for (let j = 0; j < vowels.length; j++) {
+      if (str[i] === vowels[j]) count += 1
+    }
+  }
+
+  return count
+}
+
+module.exports = countVowels
