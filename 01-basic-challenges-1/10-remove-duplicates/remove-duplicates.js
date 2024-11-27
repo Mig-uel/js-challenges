d5a7695d4331df614c982a810693b15e85e4419d
Miguel <35431824+Mig-uel@users.noreply.github.com>
2024-11-27 10:34:16 -0500

    Refactor removeDuplicates function to use Set for improved performance and readability

--- 01-basic-challenges-1/10-remove-duplicates/remove-duplicates.js
+++ 01-basic-challenges-1/10-remove-duplicates/remove-duplicates.js
@@ -1,20 +1,19 @@
 function removeDuplicates(arr) {
-  const newArr = [arr[0]]
+  // const newArr = [arr[0]]
+  // for (let i = 0; i < arr.length; i++) {
+  //   let isDuplicate = false
+  //   for (let j = 0; j < newArr.length; j++) {
+  //     if (arr[i] === newArr[j]) {
+  //       isDuplicate = true
+  //       break
+  //     }
+  //   }
+  //   if (!isDuplicate) newArr.push(arr[i])
+  // }
+  // return newArr
 
-  for (let i = 0; i < arr.length; i++) {
-    let isDuplicate = false
-
-    for (let j = 0; j < newArr.length; j++) {
-      if (arr[i] === newArr[j]) {
-        isDuplicate = true
-        break
-      }
-    }
-
-    if (!isDuplicate) newArr.push(arr[i])
-  }
-
-  return newArr
+  // solution using sets
+  return Array.from(new Set(arr))
 }
 
 module.exports = removeDuplicates
